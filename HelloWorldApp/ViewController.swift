b494381285072ab18b8ae0f49d7a228c6580c471
ErrorV9 <vlad.nikitenkov95@gmail.com>
2021-03-22 23:07:57 +0300

    work with logic

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var showTextButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        showTextButton.layer.cornerRadius = 10
+        helloWorldLabel.textColor = .systemRed
     }
 
-
+    @IBAction func showButtonPressed() {
+        helloWorldLabel.isHidden.toggle()
+        if helloWorldLabel.isHidden {
+            showTextButton.setTitle("Show", for: .normal)
+        } else {
+            showTextButton.setTitle("Hidee Text", for: .normal)
+        }
+    }
+    
 }
 
